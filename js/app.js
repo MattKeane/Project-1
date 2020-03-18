ea4868deb7ea7a15f0db7642b74ae1b7b32e4a53
Matt Keane <mattpkeane@gmail.com>
2020-03-18 10:55:51 -0500

    created Wall calss extending Tile

--- js/app.js
+++ js/app.js
@@ -2,23 +2,42 @@ const board = document.querySelector("#board")
 const ctx = board.getContext("2d")
 
 class Tile {
-	constructor(x, y) {
+	constructor(x, y, passable) {
 		this.x = x
 		this.y = y
+		this.passable = passable
 	}
 }
 
 class Floor extends Tile {
 	constructor(x, y) {
-		super(x, y)
+		super(x, y, true)
 	}
 	draw() {
 		ctx.beginPath()
 		ctx.rect(this.x * 25, this.y * 25, 25, 25)
-		ctx.fillStyle = "black"
+		ctx.fillStyle = "rgb(200, 200, 200)"
 		ctx.fill()
+	}
+}
+
+class Wall extends Tile {
+	constructor(x,y) {
+		super(x, y, false)
+	}
+	draw () {
+		ctx.beginPath()
+		ctx.rect(this.x * 25, this.y * 25, 25, 25)
+		ctx.fillStyle = "rgb(100, 100, 100)"
+		ctx.fill()
+		ctx.rect(this.x * 25, this.y * 25, 25, 25)
+		ctx.lineWidth = 1
+		ctx.strokeStyle = "black"
+		ctx.stroke()
 	}
 }
 
 const tile1 = new Floor(0, 0)
 const tile2 = new Floor(3, 4)
+const tile3 = new Floor(0, 1)
+const tile4 = new Wall(1, 0)
